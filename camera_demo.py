4d4690cd3c13e212d62014458918cc2ebce70f27
jiangchangliang@live.com <jiangchangliang@live.com>
2022-09-29 05:48:16 +0000

    修改ip

--- camera_demo.py
+++ camera_demo.py
@@ -1,12 +1,12 @@
 import get_android
 #拉流
-get_android.androidOb("ip_camera_config.json",1,960,540,4)
+# get_android.androidOb("ip_camera_config.json",1,960,540,4)
 #原生
 # get_android.androidOb("native_config.json",4,1920,1080,1)
 #usb
 # get_android.androidOb("uvc_config.json",6,1920,1080,1)
 #拉流推流
-# get_android.androidOb("ip_camera_encode_config.json",2,960,540,4)
+get_android.androidOb("ip_camera_encode_config.json",2,960,540,4)
 #拉流推流存储
 # get_android.androidOb("ip_camera_save_config.json",3,960,540,4)
 #原生推流
